fd23158833a2d36af5588dc8dbd4c2facc20aa80
paoloo <paolo@fisica.ufc.br>
2016-05-09 00:54:32 -0300

    projeto inicial gerado pelo compojure

--- project.clj
+++ project.clj
@@ -1,1 +1,12 @@
-(defproject trabalhofinal "0.1.0-SNAPSHOT")
+(defproject funcprogifce "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler funcprogifce.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
