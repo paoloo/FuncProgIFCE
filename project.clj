644a2bce9f006725f9c83b6a634d31b014648aaf
paoloo <paolo@fisica.ufc.br>
2016-05-09 01:06:23 -0300

    adicao do suporte completo a JSON

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject funcprogifce "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Apoio a disciplina de paradigmas de programacao"
+  :url "https://github.com/paoloo/FuncProgIFCE"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.1.2"]
+                 [cheshire "4.0.3"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler funcprogifce.handler/app}
   :profiles
--- src/funcprogifce/handler.clj
+++ src/funcprogifce/handler.clj
@@ -1,11 +1,16 @@
 (ns funcprogifce.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.middleware.json :as middleware]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] {:body {:text "Hello World"}})
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+(-> (handler/api app-routes)
+    (middleware/wrap-json-body)
+    (middleware/wrap-json-params)
+    (middleware/wrap-json-response)))
